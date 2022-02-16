505d0b3cc9296b2b0caef725a9d8020f0c93da5c
gaurav singh <93646503+Gauravgit48@users.noreply.github.com>
2022-02-16 13:29:12 +0530

    Add files via upload
    adding files

---
+++ script.js
@@ -0,0 +1,8 @@
+var  navlinks=document.getElementById('navLinks')
+   function showmenu(){
+       navlinks.style.right="0";
+
+   }
+   function hidemenu(){
+       navlinks.style.right="-200px";
+   }
